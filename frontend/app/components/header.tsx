c2fbb9e5791b094621a45c0c2e62a5b3f9f079be
Cauliflower2028 <colinlee2024@gmail.com>
2025-10-26 11:29:45 -0400

    ver

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -42,7 +42,7 @@ export default function Header() {
           </svg>
         </button>
         <button className="px-6 py-2 rounded-full bg-white text-black font-normal text-xs transition-all duration-300 hover:bg-white/90 h-8 flex items-center z-10 cursor-pointer">
-          Download
+          Meet the Team
         </button>
       </div>
 
